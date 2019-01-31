b3053952ea565cc7f89ec828fc1eab4e8a67df64
svyborov <sergey-vyborov@yandex.ru>
2019-01-31 22:07:26 +0300

    add depends to func loadpage

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -9,20 +9,30 @@ import loadPage from '../src';
 const fsPromises = fs.promises;
 axios.defaults.adapter = httpAdapter;
 const pathToFixture = './__tests__/__fixtures__/';
+const testFile = 'cdn-cgi-scripts-5c5dd728-cloudflare-static-email-decode-min.js';
+// const testFile2 = 'courses.html';
 const fileName = 'hexlet-io-courses.html';
 let pathToTemp;
 let htmlData;
+let testData;
 
 beforeEach(async () => {
   pathToTemp = await fsPromises.mkdtemp(`${os.tmpdir()}${path.sep}`);
   htmlData = await fsPromises.readFile(path.resolve(pathToFixture, fileName));
+  console.log(`${pathToFixture}hexlet-io-courses_files/`);
+  testData = await fsPromises.readFile(path.resolve(`${pathToFixture}hexlet-io-courses_files/`, testFile));
   nock('https://hexlet.io/')
     .get('/courses')
-    .reply(200, htmlData);
+    .reply(200, htmlData)
+    .get('cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js')
+    .reply(200, testData);
 });
+
 
 test('test https://hexlet.io/courses', async () => {
   await loadPage('https://hexlet.io/courses', pathToTemp);
   const expectData = await fsPromises.readFile(path.resolve(pathToTemp, fileName));
+  const expectDataN = await fsPromises.readFile(path.resolve(`${pathToTemp}/hexlet-io-courses_files/`, testFile));
   expect(expectData.toString()).toEqual(htmlData.toString());
+  expect(expectDataN.toString()).toEqual(testData.toString());
 });
--- src/page-loader.js
+++ src/page-loader.js
@@ -6,28 +6,82 @@ import cheerio from 'cheerio';
 
 const fsPromises = fs.promises;
 
+const tagsTypes = {
+  script: 'src',
+  link: 'href',
+  img: 'src',
+};
+
 const normilizefileName = (address) => {
+  // console.log(address);
+  let ext = path.extname(address);
   const { hostname, pathname } = url.parse(address);
-  const fileName = `${hostname}${pathname}`.replace(/\W/g, '-');
-  return fileName.includes('.html') ? fileName : `${fileName}.html`;
+  // console.log(ext);
+  if (!ext) {
+    ext = '.html';
+  }
+  // console.log('AAAAAAAA', hostname, 'AAAAAAAAAA', pathname);
+  const fileName = `${hostname || ''}${pathname}`.replace(ext, '').replace(/\W/g, '-');
+  return { fileName: `${fileName}${ext}`, pathToFiles: `${fileName}_files` };
 };
 
 const loadPage = (address, pathToSave = '') => {
-  const fileName = normilizefileName(address);
-  axios.defaults.host = url.parse(address).host;
+  const { fileName, pathToFiles } = normilizefileName(address);
+  console.log(fileName);
+  const { host, protocol } = url.parse(address);
+  // console.log(url.parse(address));
+  // console.log(path.resolve(pathToSave, '__fixures'));
+  axios.defaults.host = host;
   let res;
+  let $;
   return axios.get(address)
     .then((response) => {
-      fsPromises.writeFile(path.resolve(pathToSave, fileName), response.data);
+      // console.log(response);
+      // fsPromises.writeFile(path.resolve(pathToSave, fileName), response.data);
       res = response;
     })
+    .then(() => fsPromises.mkdir(path.resolve(pathToSave, pathToFiles)))
     .then(() => {
-      const $ = cheerio.load(res.data);
-      const tags = $('script, link, img');
-      console.log(tags);
+      $ = cheerio.load(res.data);
+      const tags = $('script[src^="/"], link[href^="/"], img[src^="/"]');
+      // console.log(tags);
+      // const links = [];
+      tags.each((i, el) => {
+        console.log('EL', el);
+        console.log('ЗНАЧЕНИЕ АТРИБУТА', $(el).attr(tagsTypes[el.name]));
+        // links.push($(el).attr(tagsTypes[el.name]));
+        const lin = $(el).attr(tagsTypes[el.name]);
+        const localAdress = new URL(lin, address).href; // `${protocol}//${host}${l}`;
+        // console.log(localAdress);
+        const localFileName = normilizefileName(lin).fileName.slice(1);
+        console.log(localFileName);
+        const newPath = path.resolve(pathToSave, pathToFiles, localFileName);
+        console.log('%%%%%%% ПУТЬ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%', newPath);
+        return axios.get(localAdress)
+          .then(response => fsPromises.writeFile(newPath, response.data))
+          .then(() => $(el).attr(tagsTypes[el.name], newPath))
+          .then(() => console.log('НОВАЯ ССЫЛКА', $(el).attr(tagsTypes[el.name])))
+          .then(() => fsPromises.writeFile(path.resolve(pathToSave, fileName), $.html()))
+          .catch(console.log);
+      });/*
+      console.log(links);
+      links.map((l) => {
+        console.log(host, '&&&&&&', l);
+        console.log(new URL(l, address));
+        const localAdress = new URL(l, address).href; // `${protocol}//${host}${l}`;
+        // console.log(localAdress);
+        const localFileName = normilizefileName(l).fileName.slice(1);
+        console.log(localFileName);
+        const newPath = path.resolve(pathToSave, pathToFiles, localFileName);
+        return axios.get(localAdress)
+          .then(response => fsPromises.writeFile(newPath, response.data))
+          .then(() => )
+          .catch(console.log);
+      }); */
     })
-    .catch((error) => {
-      throw error;
+    .then(() => {
+      // console.log($.html());
+      return fsPromises.writeFile(path.resolve(pathToSave, fileName), $.html());
     });
 };
 
